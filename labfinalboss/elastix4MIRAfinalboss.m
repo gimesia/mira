8d94c77d8511fe4afece9fb6c4511c86264150a6
Aron Gimesi <gimesiaron@gmail.com>
2024-12-05 16:48:57 +0100

    wip(labfinalboss): update matlab pipeline

--- labfinalboss/elastix4MIRAfinalboss.m
+++ labfinalboss/elastix4MIRAfinalboss.m
@@ -1,17 +1,29 @@
-function read_raw_binary_to_nifti(filename, shape, spacing)
+function nii = read_raw_to_nifti(filename, shape, spacing, dest)
     % Read binary data
     fid = fopen(filename, 'rb');
-    data = fread(fid, inf, 'int16');
+    data = fread(fid, inf, 'int16','ieee-le'); % little-endian unsigned 16bit integer
     fclose(fid);
 
-    % Reshape data
+    size(data)
+    shape(1)*shape(2)*shape(3)
+
     data = reshape(data, shape);
 
     % Create NIfTI image
-    img = make_nii(data, -spacing);
+    nii = make_nii(data, spacing, [0,0,0], 4);
+
+    nii.hdr.hist.sform_code = 1;  % Use a valid sform
+    nii.hdr.hist.srow_x = [-spacing(1) 0 0 0];  % spacing along x-axis
+    nii.hdr.hist.srow_y = [0 -spacing(2) 0 0];  % spacing along y-axis
+    nii.hdr.hist.srow_z = [0 0 -spacing(3) 0];  % spacing along z-axis
 
     % Save the NIfTI image
-    save_nii(img, strrep(filename, '.img', '_fromMATLAB.nii.gz'));
+    dstPath = strrep(filename, '.img', '.nii')
+    if ~ischar(dstPath) % && ~isstring(dstPath)
+        dstPath = char(dstPath); % Convert to char if needed
+    end
+    save_nii(nii, dstPath);
+    nii;
 end
 
 % Paths
@@ -19,12 +31,9 @@ elastixBasePath = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\MIRA\lab\lab2\";
 basePath = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\MIRA\lab\labfinalboss\";
 dataPath = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\MIRA\lab\labfinalboss\data\";
 
-rawImgFiles = dir(fullfile(dataPath, "**", "*.img")); % only raw
-rawImgFiles = {rawImgFiles.name}
-
-for i = rawImgFiles
-    
-end
+rawImgFiles = dir(fullfile(dataPath, "**", "*BHCT.img")); % only raw
+rawImgFiles = {rawImgFiles.name};
+rawImgFiles = strcat(strtok(rawImgFiles, '_'), '\', rawImgFiles);
 
 imgFiles = dir(fullfile(dataPath, "**", "*.nii.gz")); % only nifti
 imgFiles = {imgFiles.name};  % Get all filenames in a cell array
@@ -42,29 +51,44 @@ cases = unique(strtok(pointFiles, "\"));
 parameterFileBSpline = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\" + ...
     "MIRA\lab\lab2\elastix_model_zoo\models\Par0016\" + ...
     "Par0016.multibsplines.lung.sliding.txt";
-    
+
 % Paths to elastix and transformix executables
 elastixPath = elastixBasePath + "elastix-5.0.0-win64\elastix.exe";
 transformixPath = elastixBasePath + "elastix-5.0.0-win64\transformix.exe";
 
 
-for caseID = 1:length(cases)
-    caseName = cases{caseID};
-    disp("Processing Case [" + caseName + "]")
-    idxs = cellfun(@(x) contains(x, caseName), imgFiles);
+for caseID = 1:1 %length(cases)
+    caseName = string(cases{caseID});
+    disp("Processing Case [" + caseName + "]");
+    idxs = cellfun(@(x) contains(x, caseName), rawImgFiles);
     idxs = find(idxs == 1);
-    caseImgs = imgFiles(idxs);
+    caseImgs = rawImgFiles(idxs);
     casePoints = pointFiles(idxs);
+    caseMetaData = metaData(metaData.Label == caseName, :);
+    imageDims = caseMetaData.ImageDims;
+    imageSpacing = caseMetaData.Spacing;
 
+    % Moving Img
     inhaleImgId = cellfun(@(x) contains(x, "iBH"), caseImgs);
-    exhaleImgId = cellfun(@(x) contains(x, "eBH"), caseImgs);
-    inhaleImg= fullfile(dataPath, caseImgs{inhaleImgId == 1});
-    exhaleImg = fullfile(dataPath, caseImgs{exhaleImgId == 1});
+    inhaleImg = fullfile(dataPath, caseImgs{inhaleImgId == 1})
+    inhaleOut = read_raw_to_nifti(inhaleImg, imageDims, imageSpacing, inhaleOut);
+    inhaleImg
 
+    % Fixed Img
+    exhaleImgId = cellfun(@(x) contains(x, "eBH"), caseImgs);
+    exhaleImg = fullfile(dataPath, caseImgs{exhaleImgId == 1})
+    exhaleOut = read_raw_to_nifti(exhaleImg, imageDims, imageSpacing, exhaleOut);
+    exhaleImg
+
+    % Fixed Points
     inhalePointsId = cellfun(@(x) contains(x, "iBH"), casePoints);
-    exhalePointsId = cellfun(@(x) contains(x, "eBH"), casePoints);
     inhalePoints = fullfile(dataPath, casePoints{inhalePointsId == 1});
+    inhalePointsData = readmatrix(inhalePoints);
+
+    % Moving Points
+    exhalePointsId = cellfun(@(x) contains(x, "eBH"), casePoints);
     exhalePoints = fullfile(dataPath, casePoints{exhalePointsId == 1});
+    exhalePointsData = readmatrix(inhalePoints);
 
     outputDirectory = basePath + "data\" + caseName + "\out";
     if ~exist(outputDirectory, 'dir')
@@ -85,113 +109,7 @@ end
 
 
 
-% Moving
-movingImage = basePath + "result-atlas2\atlas_intensity.nii.gz";
 
 
-for caseId = 1:length(shortFileNames)
-    realCurrentMovingImage = string(shortFileNames(caseId));
 
-    baseOutputDirectory = basePath + "output2\" + realCurrentMovingImage + "\";
-    disp("Processing Image No. " + realCurrentMovingImage);
-    % Fixed
-    fixedImage = basePath + "test-set\testing-images\" + realCurrentMovingImage + ".nii.gz";
-    fixedMask = basePath + "test-set\testing-mask\" + realCurrentMovingImage + "_1C.nii.gz";
-    fixedLabels = basePath + "test-set\testing-labels\" + realCurrentMovingImage + "_3C.nii.gz";
-    
-    
-    % Create output directory for affine registration
-    outputDirectoryAffine = baseOutputDirectory + "registration";
-    if ~exist(outputDirectoryAffine, 'dir')
-        mkdir(outputDirectoryAffine);
-    end
-    
-  
-    % Start affine registration process
-    disp('RUNNING REGISTRATION PIPELINE FOR CASE: ' + realCurrentMovingImage)
-    elastixCommand = sprintf('"%s" -f "%s" -m "%s" -fMask "%s" -p "%s" -p "%s" -out "%s"', ...
-                      elastixPath, fixedImage, movingImage, fixedMask, parameterFileAffine, parameterFileBSpline, outputDirectoryAffine);
-    [status, result] = system(elastixCommand);
-    
-    if status == 0
-        % Affine registration succeeded, renaming output file
-        elastixResultFile = fullfile(outputDirectoryAffine, "result.0.nii.gz");
-        newElastixResultFileName = fullfile(baseOutputDirectory, realCurrentMovingImage + "_registered_image_" + "_affine.nii.gz");
-        if isfile(elastixResultFile)
-            movefile(elastixResultFile, newElastixResultFileName);
-        end
-        affineTransformationParamFile = fullfile(outputDirectoryAffine, 'TransformParameters.0.txt');
-        
-        % Affine registration succeeded, renaming output file
-        elastixResultFile = fullfile(outputDirectoryAffine, "result.1.nii.gz");
-        newElastixResultFileName = fullfile(baseOutputDirectory, realCurrentMovingImage + "_registered_image_" + "_bspline.nii.gz");
-        
-        if isfile(elastixResultFile)
-            movefile(elastixResultFile, newElastixResultFileName);
-        end
-        transformationParamFile = fullfile(outputDirectoryAffine, 'TransformParameters.1.txt')
-        
-        disp('Elastix registration completed successfully.');
-    else
-        disp('Error running Elastix');
-        disp(result);
-        return; % Exit if affine registration fails
-    end
-    
-    
-    % Modifying transformation parameter files
-    % List of transformation parameter files to modify
-    transformParamFiles = {affineTransformationParamFile, transformationParamFile};
-    for i = 1:length(transformParamFiles)
-        currentFile = transformParamFiles{i};
-        
-        % Check if the parameter file exists
-        if isfile(currentFile)
-            % Read the file content
-            fileText = fileread(currentFile);
-    
-            % Replace the interpolator with "FinalNearestNeighborInterpolator"
-            fileText = regexprep(fileText, ...
-                '\(ResampleInterpolator\s+"FinalBSplineInterpolator"\)', ...
-                '(ResampleInterpolator "FinalNearestNeighborInterpolator")');
-            
-            % Replace ResultImagePixelType with "float"
-            fileText = regexprep(fileText, ...
-                '\(ResultImagePixelType\s+".*?"\)', ...
-                '(ResultImagePixelType "float")');
-            
-            % Write the modified content back to the file
-            fileID = fopen(currentFile, 'w');
-            fprintf(fileID, '%s', fileText);
-            fclose(fileID);
-        else
-            disp(['Transformation parameter file not found: ', currentFile]);
-        end
-    end
-    
-    % Setup output directories for Transformix
-    outputTransformixLabels = fullfile(baseOutputDirectory, "transformix");
-    if ~exist(outputTransformixLabels, 'dir')
-        mkdir(outputTransformixLabels);
-    end
-    
-    for i = 0:3
-        movingLabel = basePath + "\result-atlas2\atlas_probability_label_" + string(num2str(i)) + ".nii.gz"
-        % Run Transformix for mask transformation
-        transformixMaskCommand = sprintf('"%s" -in "%s" -tp "%s" -out "%s"', ...
-                             transformixPath, movingLabel, transformationParamFile, outputTransformixLabels);
-        [statusMask, resultMask] = system(transformixMaskCommand);
-    
-        % Rename transformed mask file
-        transformedMaskFile = fullfile(outputTransformixLabels, "result.nii.gz");
-        newMaskFileName = fullfile(baseOutputDirectory, realCurrentMovingImage + "_transformed_atlas_label_" + string(num2str(i)) + ".nii.gz");
-        if isfile(transformedMaskFile)
-            movefile(transformedMaskFile, newMaskFileName);
-        end
-        if statusMask == 0
-            disp("Probability-map for label " + string(i) + " transformed successfully")
-        else
-            disp("Probability-map for label " + string(i) + " transformation failed")
-        end
-    end
-end
+
