a8948fc6f071c1e67c98ac74c80d06b4ef6cee09
Aron Gimesi <gimesiaron@gmail.com>
2024-12-09 00:26:36 +0100

    feat(labfinalboss): preprocessing

--- labfinalboss/matlab/elastixMIRAfinalboss.m
+++ labfinalboss/matlab/elastixMIRAfinalboss.m
@@ -4,13 +4,10 @@ function nii = read_raw_to_nifti(filename, shape, spacing)
     data = fread(fid, inf, 'int16','ieee-le'); % little-endian unsigned 16bit integer
     fclose(fid);
 
-    size(data);
-    shape(1)*shape(2)*shape(3);
-
     data = reshape(data, shape);
 
     % Create NIfTI image
-    nii = make_nii(data, spacing, [0,0,0], 4);
+    nii = make_nii(data, spacing);
 
     nii.hdr.hist.sform_code = 1;  % Use a valid sform
     nii.hdr.hist.srow_x = [-spacing(1) 0 0 0];  % spacing along x-axis
@@ -44,13 +41,48 @@ function maskNifti = create_binary_mask_nifti(points, templateNifti)
     mask = create_binary_mask(points, dims);
 
     % Dilate (optionally)
-    se = strel("cube", 5);
-    %mask = imdilate(mask, se);
+    se = strel("cube", 3);
+    mask = imdilate(mask, se);
 
     maskNifti = make_nii(mask, [1,1,1], [1,1,1]);
 end
 
+function showMidSlice(niftiImg)
+    midSlice = round(size(niftiImg.img,3)/2);
+    imshow(niftiImg.img(:,:,midSlice));
+end
 
+function hist(V)
+    figure
+    histogram(V)
+    xlim([min(V,[],"all") max(V,[],"all")])
+    ylim([0 2e6])
+    xlabel("Intensity")
+    ylabel("Number of Voxels")
+end
+
+function nii = preprocess(niftiImg)
+    nii = niftiImg;
+    nii.img(nii.img<0) = 0;
+    min_val = min(nii.img(:));
+    max_val = max(nii.img(:));
+    normalizedImg = (nii.img - min_val) / (max_val - min_val);
+
+    hist(niftiImg.img);
+    title("before");
+    hist(nii.img)
+    title("clipping");
+    hist(normalizedImg)
+    title("after");
+    % Update the NIfTI structure with the normalized image
+    nii.img = normalizedImg;
+
+    % Update the header information
+    nii.hdr.dime.cal_min = 0;
+    nii.hdr.dime.cal_max = 1;
+end
+
+metaData
 % Paths
 elastixBasePath = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\MIRA\lab\lab2\";
 basePath = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\MIRA\lab\labfinalboss\";
@@ -64,23 +96,22 @@ imgFiles = dir(fullfile(dataPath, "**", "*.nii.gz")); % only nifti
 imgFiles = {imgFiles.name};  % Get all filenames in a cell array
 imgFiles = strcat(strtok(imgFiles, '_'), '\', imgFiles);
 
-pointFiles = dir(fullfile(dataPath, "**", "*.txt")); % only txt
+pointFiles = dir(fullfile(dataPath, "**", "*xyz_r1.txt")); % only points txt
 pointFiles= {pointFiles.name};  % Get all filenames in a cell array
 pointFiles = strcat(strtok(pointFiles, '_'), '\', pointFiles);
 
 cases = unique(strtok(pointFiles, "\"));
 
-% Path to affine parameter file
-% parameterFileAffine = elastixBasePath + "elastix_model_zoo\models\Par0010\Par0010affine.txt";
 % Path to B-spline parameter file
-parameterFileBSpline = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\" + ...
-    "MIRA\lab\lab2\elastix_model_zoo\models\Par0016\" + ...
-    "Par0016.multibsplines.lung.sliding.txt";
+parameter = "Par0016"
+parameterFileBSpline = "C:\Users\gimes\OneDrive\MAIA\3_UdG\classes\MIRA\lab\lab2\elastix_model_zoo\models\Par0016\Par0016.multibsplines.lung.sliding.txt"
 
 % Paths to elastix and transformix executables
 elastixPath = elastixBasePath + "elastix-5.0.0-win64\elastix.exe";
 transformixPath = elastixBasePath + "elastix-5.0.0-win64\transformix.exe";
 
+% To track timing for each case
+registrationTimes = struct();
 
 for caseID = 1:1%length(cases)
     caseName = string(cases{caseID});
@@ -89,19 +120,21 @@ for caseID = 1:1%length(cases)
     idxs = find(idxs == 1);
     caseImgs = rawImgFiles(idxs);
     casePoints = pointFiles(idxs);
-    caseMetaData = metaData(metaData.Label == caseName, :);
-    imageDims = caseMetaData.ImageDims;
-    imageSpacing = caseMetaData.Spacing;
+    index = find(strcmp(data.Label, caseName));
+    imageDims = data.ImageDims{index};
+    imageSpacing = data.Spacing{index};
 
     % Moving Img
     inhaleImgId = cellfun(@(x) contains(x, "iBH"), caseImgs);
     inhaleImg = fullfile(dataPath, caseImgs{inhaleImgId == 1});
     inhaleImgNifti= read_raw_to_nifti(inhaleImg, imageDims, imageSpacing);
 
+
     % Fixed Img
     exhaleImgId = cellfun(@(x) contains(x, "eBH"), caseImgs);
     exhaleImg = fullfile(dataPath, caseImgs{exhaleImgId == 1});
     exhaleImgNifti = read_raw_to_nifti(exhaleImg, imageDims, imageSpacing);
+
 
     % Fixed Points
     inhalePointsId = cellfun(@(x) contains(x, "iBH"), casePoints);
@@ -113,48 +146,87 @@ for caseID = 1:1%length(cases)
     exhalePoints = fullfile(dataPath, casePoints{exhalePointsId == 1});
     exhalePointsData = readmatrix(exhalePoints);
 
-
     outputDirectory = basePath + "data\" + caseName + "\out";
     if ~exist(outputDirectory, 'dir')
         mkdir(outputDirectory);
     end
 
-    % Create binary masks
-    % Create binary masks using the NIfTI template
+    % Create lung masks using the NIfTI template
+    % inhaleLungMaskNifti = uNetLungMask(inhaleImgNifti);
+    % exhaleLungMaskNifti = uNetLungMask(exhaleImgNifti);
+
+
+
+
+    % Create point masks using the NIfTI template
     inhalePointsMaskNifti = create_binary_mask_nifti(inhalePointsData, inhaleImgNifti);
     exhalePointsMaskNifti = create_binary_mask_nifti(exhalePointsData, exhaleImgNifti);
+
+
+
+    % Create preprocessed NIfTI images
+    inhaleImgPreprocNifti = preprocess(inhaleImgNifti);
+    exhaleImgPreprocNifti = preprocess(exhaleImgNifti);
 
     % Saving
     [~, inhaleImgFileName, ~] = fileparts(inhaleImg);
     [~, exhaleImgFileName, ~] = fileparts(exhaleImg);
 
-
     % Inhale
     inhaleImgNiftiPath = char(fullfile(outputDirectory, ...
-        inhaleImgFileName + ".nii"))
+        inhaleImgFileName + ".nii"));
+    inhaleImgPreprocNiftiPath = char(fullfile(outputDirectory, ...
+        inhaleImgFileName + "_preproc.nii"));
     inhalePointsMaskNiftiPath = char(fullfile(outputDirectory, ...
-        inhaleImgFileName + '_keypoint_mask.nii'))
-    save_nii(inhaleImgNifti, inhaleImgNiftiPath);
-    save_nii(inhalePointsMaskNifti, inhalePointsMaskNiftiPath);
+        inhaleImgFileName + '_keypoint_mask.nii'));
+    % inhaleLungMaskNiftiPath = char(fullfile(outputDirectory, ...
+    %     inhaleImgFileName + '_mask.nii'));
 
+    save_nii(inhaleImgNifti, inhaleImgNiftiPath);
+    save_nii(inhaleImgPreprocNifti, inhaleImgPreprocNiftiPath);
+    save_nii(inhalePointsMaskNifti, inhalePointsMaskNiftiPath);
+    % save_nii(inhaleLungMaskNifti, inhaleLungMaskNiftiPath);
 
     % Exhale
     exhaleImgNiftiPath = char(fullfile(outputDirectory, ...
-        exhaleImgFileName + ".nii"))
+        exhaleImgFileName + ".nii"));
+    exhaleImgPreprocNiftiPath = char(fullfile(outputDirectory, ...
+        exhaleImgFileName + "_preproc.nii"));
     exhalePointsMaskNiftiPath = char(fullfile(outputDirectory, ...
-        exhaleImgFileName + '_keypoint_mask.nii'))
-    save_nii(exhaleImgNifti, exhaleImgNiftiPath);
-    save_nii(exhalePointsMaskNifti, exhalePointsMaskNiftiPath);
+        exhaleImgFileName + '_keypoint_mask.nii'));
+    % exhaleLungMaskNiftiPath = char(fullfile(outputDirectory, ...
+         % exhaleImgFileName + '_mask.nii'));
 
-    disp("Registration with elastix ([exhale] -> [inhale]): " + caseName);
-    elastixCommand = sprintf('"%s" -f "%s" -m "%s" -p "%s" -labels "%s" -out "%s"', ...
-                      elastixPath, exhaleImgNiftiPath, inhaleImgNiftiPath, ...
-                      parameterFileBSpline, inhalePointsMaskNiftiPath, ...
-                      outputDirectory);
+    save_nii(exhaleImgNifti, exhaleImgNiftiPath);
+    save_nii(exhaleImgPreprocNifti, exhaleImgPreprocNiftiPath);
+    save_nii(exhalePointsMaskNifti, exhalePointsMaskNiftiPath);
+    % save_nii(exhaleLungMaskNifti, exhaleLungMaskNiftiPath);
+
+    caseOutputDirectory = fullfile(outputDirectory, parameter);
+    if ~exist(caseOutputDirectory, 'dir')
+        mkdir(caseOutputDirectory);
+    end
+
+    % Measure time for registration
+    disp("Starting registration with elastix ([exhale] -> [inhale]): " + caseName);
+    tic;  % Start timing
+    elastixCommand = sprintf('"%s" -f "%s" -m "%s" -p "%s"  -labels "%s" -out "%s"', ...
+                      elastixPath, exhaleImgPreprocNiftiPath, inhaleImgPreprocNiftiPath, ...
+                      parameterFileBSpline, ...
+                      inhalePointsMaskNiftiPath, caseOutputDirectory);
     [status, result] = system(elastixCommand);
+    elapsedTime = toc;  % End timing
+
+    % Store the time
+    registrationTimes.(caseName) = elapsedTime;
+
     if status == 0
-        disp("Registration with elastix completed.")
+        disp("Registration with elastix completed in " + elapsedTime + " seconds.");
     else
-        disp("There has been an error, see log!")
+        disp("There has been an error, see log!");
     end
 end
+
+% Display summary of registration times
+disp("Registration times for all cases:");
+disp(registrationTimes);
